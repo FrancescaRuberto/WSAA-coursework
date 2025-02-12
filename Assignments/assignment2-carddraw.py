8dfc8960224f5d5c60b7fadf674f43ad88af6fe4
Francesca Ruberto <francescaruberto01@gmail.com>
2025-02-12 19:31:15 +0000

    first try assessment

--- Assignments/assignment2-carddraw.py
+++ Assignments/assignment2-carddraw.py
@@ -3,3 +3,10 @@
 
 import requests
 
+#Shuffle a new deck
+url= ("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+response = requests.get(url)
+
+# Get the deck id
+deck = response.json()
+deck_id = deck['deck']
