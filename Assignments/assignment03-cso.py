2b1fe34332331c87080c9b2765fcf679a0771710
Francesca Ruberto <francescaruberto01@gmail.com>
2025-03-19 18:31:09 +0000

    assignment week 3 get request

--- Assignments/assignment03-cso.py
+++ Assignments/assignment03-cso.py
@@ -2,3 +2,6 @@ import requests
 
 # Define the API endpoint (URL) for the required dataset
 url = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en'
+
+# Send request to get the data
+response = requests.get(url)
