77775a4b562918d3a2e1aa1c6087cdc486928f0c
Francesca Ruberto <francescaruberto01@gmail.com>
2025-03-20 17:08:34 +0000

    worked on assessment

--- Assignments/assignment03-cso.py
+++ Assignments/assignment03-cso.py
@@ -1,3 +1,6 @@
+# This program retrieves the dataset for the "exchequer account (historical series)" from the CSO 
+# And stores it into a file called "cso.json"
+
 import requests
 
 # Define the API endpoint (URL) for the required dataset
@@ -5,3 +8,4 @@ url = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ
 
 # Send request to get the data
 response = requests.get(url)
+
