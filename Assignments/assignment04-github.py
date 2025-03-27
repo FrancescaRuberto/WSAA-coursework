bcaa2f94e1cdb3fd758e491b28cc1ef404c2a7a9
Francesca Ruberto <francescaruberto01@gmail.com>
2025-03-27 17:57:35 +0000

    added repo, file name and name

--- Assignments/assignment04-github.py
+++ Assignments/assignment04-github.py
@@ -1,4 +1,9 @@
 from github import Github
 from config import config as cfg
 
+# Configuration of my name and repository
+# I created a new public repository with a file called test.txt in it
+my_name = "Francesca"
+repository_name = "FrancescaRuberto/Miscellaneous"
+file_path = "test.txt"
 
