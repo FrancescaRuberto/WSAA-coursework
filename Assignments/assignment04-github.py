d824e0695683b39391d0527dfcc4fa1c55ff7b7a
Francesca Ruberto <francescaruberto01@gmail.com>
2025-03-28 17:37:59 +0000

    hidden and added token

--- Assignments/assignment04-github.py
+++ Assignments/assignment04-github.py
@@ -1,9 +1,13 @@
 from github import Github
-from config import config as cfg
+from config import apiKey
 
 # Configuration of my name and repository
 # I created a new public repository with a file called test.txt in it
 my_name = "Francesca"
 repository_name = "FrancescaRuberto/Miscellaneous"
 file_path = "test.txt"
+
+# I add the key already "hidden"
+g = Github(apiKey)
+repo = g.get_repo(repository_name)
 
