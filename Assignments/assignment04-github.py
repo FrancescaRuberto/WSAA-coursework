7dc91e5a9b08391da8a83516622832e33fce5e66
Francesca Ruberto <francescaruberto01@gmail.com>
2025-03-30 19:08:25 +0100

    fixed issue with changes commitment, finalized code

--- Assignments/assignment04-github.py
+++ Assignments/assignment04-github.py
@@ -13,8 +13,17 @@ repo = g.get_repo(repository_name)
 
 # I get the content of the file
 file_info = repo.get_contents(file_path)
-original_content = file_info.decoded_content.decoded("utf-8")
+original_content = file_info.decoded_content.decode("utf-8")
 
 # I now replaces all the "Andrews" in the file with my name
 updated_content = original_content.replace("Andrew", my_name)
 
+# Commit changes to my repository
+repo.update_file(
+    path=file_path,
+    message="Replaced 'Andrew' with 'Francesca",
+    content=updated_content,
+    sha=file_info.sha # added position argument after having faced an error
+    )
+
+print(f"File updated. All 'Andrews' have been replaced with 'Francesca'.")
